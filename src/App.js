0fe213059aebaba1080b505d47ae84b5f3f6bb6a
Hogor007 <asifpenta@gmail.com>
2022-03-26 23:49:08 +0600

    added readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
+import FAQ from './Components/FAQ/FAQ';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
+      <FAQ></FAQ>
+      <Footer></Footer>
     </div>
   );
 }
